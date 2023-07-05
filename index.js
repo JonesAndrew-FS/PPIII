4349b963062fb57f3cd6310d2b1df0d70590b80e
Andrew Jones <ADJones7@student.fullsail.edu>
2023-07-05 15:13:52 -0400

    mongodb implementation (mysql was mean)

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
 app.use(express.json());
 
 console.log(process.env)
+
+mongoose.connect('mongodb://127.0.0.1:27017/PPIII').then(
+    () => {console.log("it works :)))")},
+    err => {console.log(err.message)}
+);
 
 app.listen(3001);
